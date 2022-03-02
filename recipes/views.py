cc3ce739471b1ba8f385ef3513c44a5a37c8fb89
Thyago Mendes <thyagosme@gmail.com>
2022-03-02 13:36:41 -0300

    Criando header e separando um "partial" para head

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html', context={'name': 'Thyago Mendes'})
+    return render(request,'recipes/pages/home.html', context={'name': 'Thyago Mendes'})
 
 
