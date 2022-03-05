5f9e63ac59e20bf505144683f495b111dfea094d
Thyago Mendes <thyagosme@gmail.com>
2022-03-05 14:00:01 -0300

    Nome único nas URLs e app_name para Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,10 @@ from recipes.views import home
 
 from . import views
 
+app_name = 'recipes'
 urlpatterns = [
 
-    path('', views.home),
-   path('recipes/<int:id>/', views.recipe),
+    path('', views.home,name='home'),
+   path('recipes/<int:id>/', views.recipe, name='recipe'),
 
 ]
