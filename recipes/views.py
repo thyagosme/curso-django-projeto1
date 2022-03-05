4cc20f36dcddf05a95a63caac90f87cae440cb0b
Thyago Mendes <thyagosme@gmail.com>
2022-03-05 12:31:12 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/pages/home.html', context={'name': 'Thyago Mendes'})
+    return render(request,'recipes/pages/home.html', context={'recipes':
+        [make_recipe() for _ in range(10)]})
 
 
 def recipe(request, id):
-    return render(request,'recipes/pages/recipe-view.html', context={'name': 'Thyago Mendes'})
+    return render(request,'recipes/pages/recipe-view.html', context={'recipe':
+       make_recipe()})
 
