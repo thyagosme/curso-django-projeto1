350bbfbb04493b642b53fd7f130ad0c6a6a9c58c
Thyago Mendes <thyagosme@gmail.com>
2022-03-05 21:14:33 -0300

    Adicionando Recipes na admin do Django

--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Category(models.Model):
       name = models.CharField(max_length=65)
       
+      def __str__(self):
+          return self.name
+      
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -24,7 +27,13 @@ class Recipe(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     
+    def __str__(self):
+          return self.title
     
     
+if __name__ == '__main__':
+    print('##########################')
     
+#    pprint(Category())
+
     
