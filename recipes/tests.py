9057853fd96f96b220a4067b90bbd04625d9e67a
Thyago Mendes <thyagosme@gmail.com>
2022-03-06 17:20:01 -0300

    Iniciando com testes no Django com Pytest, Pytest-Django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = '123456'
+        print('OLÁ MUNDO')
+        assert 1==1 
