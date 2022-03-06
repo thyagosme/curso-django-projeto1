018b9f1d587f08411860969292559e6a9ddbfc18
Thyago Mendes <thyagosme@gmail.com>
2022-03-05 22:58:59 -0300

    Usando o model Recipe real (com QuerySet) na view home

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -23,7 +25,7 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     
--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Category, Recipe
+
 
 # Create your views here.
-def home(request):
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
     return render(request,'recipes/pages/home.html', context={'recipes':
-        [make_recipe() for _ in range(10)]})
+        recipes})
+
+    # return render(request,'recipes/pages/home.html', context={'recipes':
+    #     [make_recipe() for _ in range(10)]})
+def home(request):
+    recipes = Recipe.objects.all().order_by()
+    return render(request,'recipes/pages/home.html', context={'recipes':
+        recipes})
 
 
 def recipe(request, id):
