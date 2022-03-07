60ddfe625128afd73b72f1641b2e697bcb51a6cb
Thyago Mendes <thyagosme@gmail.com>
2022-03-07 11:18:58 -0300

    Testando Django Reverse URL com argumentos args e kwargs

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,13 +4,24 @@ from django.urls import reverse
 # Create your tests here.
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = '123456'
-        print('OLÁ MUNDO')
-        assert 1==1 
-        
-        
+
+              
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url,'/')
+        url = reverse('recipes:home')
+        self.assertEqual(url,'/')
+        
+        
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id':1})
+        print(f'url: {url}')
+        
+        self.assertEqual(url,'/recipes/category/1/')
+        
+    def tests_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id':10})
+        print(f'url: {url}')
+        
+        self.assertEqual(url,'/recipes/10/')
+    
+
     
