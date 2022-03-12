88999aa8c80294640174640af70ec218d416ba8a
Thyago Mendes <thyagosme@gmail.com>
2022-03-12 18:22:57 -0300

    Criando um novo app chamado authors para usuários se registrarem

---
+++ authors/urls.py
@@ -0,0 +1,12 @@
+from aiohttp import request
+from django.urls import path
+
+from . import views
+
+app_name = 'authors'
+urlpatterns = [
+
+    path('register/', views.register_view, name='register'),
+  
+  
+]
