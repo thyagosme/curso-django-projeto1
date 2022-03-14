088d4d2be9660456fa159e9c2197540886733f84
Thyago Mendes <thyagosme@gmail.com>
2022-03-13 23:55:26 -0300

    Testando label com teste unitário

--- authors/forms.py
+++ authors/forms.py
@@ -49,13 +49,15 @@ class RegisterForm(forms.ModelForm):
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label= 'Password'
     )
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Repeat your password'
-        })
+        }),
+        label= 'Password2'
     )
 
     class Meta:
@@ -85,29 +87,9 @@ class RegisterForm(forms.ModelForm):
         }
    
 
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
+  
 
-        if 'atenção' in data:
-            raise ValidationError(
-                'Não digite %(pipoca)s no campo password',
-                code='invalid',
-                params={'pipoca': '"atenção"'}
-            )
 
-        return data
-
-    def clean_first_name(self):
-        data = self.cleaned_data.get('first_name')
-
-        if 'John Doe' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo first name',
-                code='invalid',
-                params={'value': '"John Doe"'}
-            )
-
-        return data
 
     def clean(self):
         cleaned_data = super().clean()
