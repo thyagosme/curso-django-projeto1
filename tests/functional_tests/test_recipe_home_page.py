aee9e06b8f00815eaa4d400f6cc3c8e2adcfa8c1
Thyago Mendes <thyagosme@gmail.com>
2022-03-16 21:20:46 -0300

    Movendo repetições para setUp e tearDown

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -5,14 +5,23 @@ from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
-    def sleep(self, seconds=3):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds=2):
         time.sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+        
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
         self.sleep()
-        body = browser.find_element(By.TAG_NAME , 'body')
+        body = self.browser.find_element(By.TAG_NAME , 'body')
         self.assertIn('No recipes found here 🥲', body.text)
-        browser.quit()
+     
