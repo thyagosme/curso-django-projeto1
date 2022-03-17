b021c44f9eb6b2c93b53d810d1a838b3e5036936
Thyago Mendes <thyagosme@gmail.com>
2022-03-17 20:06:26 -0300

    Teste funcional para paginação com acessibilidade (ARIA)

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -9,21 +9,17 @@ from .base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
     
         self.browser.get(self.live_server_url)
-        self.sleep()
+
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 🥲', body.text)
         
         
-    @pytest.mark.functional_test
-    def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.browser.get(self.live_server_url)
-        self.sleep()
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here 🥲', body.text)
+  
 
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
@@ -37,7 +33,7 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
 
         # Usuário abre a página
         self.browser.get(self.live_server_url)
-        self.sleep()
+       
 
 
         # Vê um campo de busca com o texto "Search for a recipe"
@@ -49,12 +45,34 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         # Clica neste input e digita o termo de busca
         # para encontrar a receita o título desejado
         search_input.send_keys(title_needed)
-        self.sleep()
+      
         search_input.send_keys(Keys.ENTER)
-        self.sleep()
+     
 
         # O usuário vê o que estava procurando na página
         self.assertIn(
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+   
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+      
+        # Vê que tem uma paginação e clica na página 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        
+        page2.click()
+        
+
+        # Vê que tem mais 2 receitas na página 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME , 'recipe')),
+            2
         )
