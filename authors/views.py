462a1bddd5f63238a10c77dcaaf0b3c4caee9ec0
Thyago Mendes <thyagosme@gmail.com>
2022-03-19 14:33:56 -0300

    Criando o formulário de criação e edição de receitas

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from recipes.models import Recipe
 
+from authors.forms.recipe_form import AuthorRecipeForm
+
 from .forms import LoginForm, RegisterForm
 
 
@@ -106,15 +108,20 @@ def dashboard_recipe_edit(request, id):
         is_published=False,
         author=request.user,
         pk=id,
-    )
+    ).first()
 
     if not recipe:
         raise Http404()
+    
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
 
     return render(
         request,
         'authors/pages/dashboard_recipe.html',
-        context={
+        context={'form': form
 
         }
     )
